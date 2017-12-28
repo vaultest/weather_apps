1b2d01ec6b430a85ea1fbdbc749b090e69798a0d
vaultest <thevaultest@gmail.com>
2017-12-28 12:15:32 +0200

    added weather main screen & logic

---
+++ app/src/main/java/com/vault/donotgetwet/model/weather/Current.java
@@ -0,0 +1,127 @@
+package com.vault.donotgetwet.model.weather;
+
+import com.vault.donotgetwet.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Vault on 26.12.2017.
+ */
+
+public class Current {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId() {
+        // values: clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night.
+        int iconId;
+        switch (mIcon) {
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+            default:
+                iconId = R.drawable.clear_day;
+                break;
+        }
+        return iconId;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() * 1000);
+        String timeString = formatter.format(dateTime);
+        return timeString;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public long getTemperature() {
+        return Math.round(mTemperature);
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public long getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return Math.round(precipPercentage);
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+}
--- app/src/main/java/com/vault/donotgetwet/view/mainscreen/MainActivity.java
+++ app/src/main/java/com/vault/donotgetwet/view/mainscreen/MainActivity.java
@@ -6,12 +6,26 @@ import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.vault.donotgetwet.R;
+import com.vault.donotgetwet.model.weather.Current;
+import com.vault.donotgetwet.model.weather.Day;
+import com.vault.donotgetwet.model.weather.Forecast;
+import com.vault.donotgetwet.model.weather.Hour;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -27,14 +41,49 @@ public class MainActivity extends AppCompatActivity {
     private static double latitude = 37.8267;
     private static double longitude = -122.4233;
     private static final String FORECAST_URL = BASE_URL + API_KEY + "/" + latitude + "," + longitude;
+    private Forecast mForecast;
+
+    @BindView(R.id.locationLabel)
+    TextView mLocationLabel;
+    @BindView(R.id.temperatureLabel)
+    TextView mTemperatureLabel;
+    @BindView(R.id.timeLabel)
+    TextView mTimeLabel;
+    @BindView(R.id.humidityValue)
+    TextView mHumidityValue;
+    @BindView(R.id.precipValue)
+    TextView mPrecipValue;
+    @BindView(R.id.summaryLabel)
+    TextView mSummaryLabel;
+    @BindView(R.id.iconImageView)
+    ImageView mIconImageView;
+    @BindView(R.id.refreshImageView)
+    ImageView mRefreshImageView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        mProgressBar.setVisibility(View.INVISIBLE);
 
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getForecast();
+            }
+        });
+
+        getForecast();
+    }
+
+    private void getForecast() {
         if (isNetworkAvailable()) {
+
+            toogleRefresh();
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder().url(FORECAST_URL).build();
             Call call = client.newCall(request);
@@ -42,25 +91,139 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onFailure(Call call, IOException e) {
                     Log.e(TAG, "onFailure caught: " + e);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toogleRefresh();
+                        }
+                    });
+                    alertUserAboutError();
                 }
 
                 @Override
                 public void onResponse(Call call, Response response) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toogleRefresh();
+                        }
+                    });
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-
+                            mForecast = parseForecastDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
+                            Log.d(TAG, "date time = " + mForecast.getCurrent().getFormattedTime());
                         } else {
                             alertUserAboutError();
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "Exception caught: " + e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "JSONException caught: " + e);
                     }
                 }
             });
         } else {
             Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void toogleRefresh() {
+        if (mProgressBar.getVisibility() == View.INVISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void updateDisplay() {
+        Current current = mForecast.getCurrent();
+        mLocationLabel.setText(current.getTimeZone());
+        mTemperatureLabel.setText(String.valueOf(current.getTemperature()));
+        mTimeLabel.setText("At " + current.getFormattedTime() + " it will be");
+        mHumidityValue.setText(String.valueOf(current.getHumidity()));
+        mPrecipValue.setText("" + String.valueOf(current.getPrecipChance()) + "%");
+        mSummaryLabel.setText(current.getSummary());
+        mIconImageView.setImageResource(current.getIconId());
+
+    }
+
+    private Forecast parseForecastDetails(String jsonData) throws JSONException {
+        Forecast forecast = new Forecast();
+        forecast.setCurrent(getCurrentDetails(jsonData));
+        forecast.setHourlyForecast(getHourlyForecast(jsonData));
+        forecast.setDailyForecast(getDailyForecast(jsonData));
+
+        return forecast;
+    }
+
+    private Day[] getDailyForecast(String jsonData) throws JSONException  {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject daily = forecast.getJSONObject("daily");
+        JSONArray data = daily.getJSONArray("data");
+
+        Day[] days = new Day[data.length()];
+        for (int i = 0; i < days.length; i++) {
+            JSONObject jsonDay = data.getJSONObject(i);
+            Day day = new Day();
+            day.setSummary(jsonDay.getString("summary"));
+            day.setTemperatureMax(jsonDay.getDouble("temperatureMax"));
+            day.setTime(jsonDay.getLong("time"));
+            day.setIcon(jsonDay.getString("icon"));
+            day.setTimeZone(timezone);
+
+            days[i] = day;
+        }
+        return days;
+    }
+
+    private Hour[] getHourlyForecast(String jsonData) throws JSONException  {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        JSONObject hourly = forecast.getJSONObject("hourly");
+        JSONArray data = hourly.getJSONArray("data");
+
+        Hour[] hours = new Hour[data.length()];
+        for (int i = 0; i < hours.length; i++) {
+            JSONObject jsonHour = data.getJSONObject(i);
+            Hour hour = new Hour();
+            hour.setSummary(jsonHour.getString("summary"));
+            hour.setTemperature(jsonHour.getDouble("temperature"));
+            hour.setIcon(jsonHour.getString("icon"));
+            hour.setTime(jsonHour.getLong("time"));
+            hour.setTimeZone(timezone);
+
+            hours[i] = hour;
+        }
+
+        return hours;
+    }
+
+    private Current getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+        JSONObject currently = forecast.getJSONObject("currently");
+        Current current = new Current();
+        current.setHumidity(currently.getDouble("humidity"));
+        current.setTime(currently.getLong("time"));
+        current.setIcon(currently.getString("icon"));
+        current.setPrecipChance(currently.getDouble("precipProbability"));
+        current.setSummary(currently.getString("summary"));
+        current.setTemperature(currently.getDouble("temperature"));
+        current.setTimeZone(timezone);
+
+        return current;
     }
 
     private boolean isNetworkAvailable() {
